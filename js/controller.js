ba524240cee7e9bca3955a87e344f87a41c25bb9
Christian Haschek <christian@haschek.at>
2020-10-14 12:36:47 +0200

    init

---
+++ js/controller.js
@@ -0,0 +1,38 @@
+$(document).ready(function () {
+    loadFloorplan(3)
+    if (location.hash != '') {
+        var page = location.hash.replace(/^#/, '');
+
+        loadHBS('/views/' + page + '.hbs', 'main', {})
+    }
+});
+
+$(window).on('hashchange', function (e) {
+    e.preventDefault();
+    var page = location.hash.replace(/^#/, '');
+    loadHBS('/views/' + page + '.hbs', 'main', {})
+});
+
+
+function loadHBS(url, rendertoelement, viewdata) {
+    $.ajax({
+        url: url,
+        cache: false,
+        success: function (data) {
+            template = Handlebars.compile(data);
+            $(rendertoelement).html(template(viewdata));
+            $(window).scrollTop($(rendertoelement).offset().top);
+        }
+    });
+}
+
+function loadFloorplan(floor)
+{
+    $.ajax({
+        url: '/'+floor+'.stock.svg',
+        cache: false,
+        success: function (data) {
+            $("#floorplan").html(new XMLSerializer().serializeToString(data.documentElement));
+        }
+    });
+}
